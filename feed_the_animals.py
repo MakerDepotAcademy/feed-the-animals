f22cbf9080dc4833d61fefdd34664fef46629ca8
Dan Habot <dan.habot@gmail.com>
2019-08-28 10:57:24 -0400

    Created preliminary game

---
+++ feed_the_animals.py
@@ -0,0 +1,16 @@
+from hole import HoleManager
+from lib import Manager
+from time import sleep
+
+Score = 0
+
+M = Manager()
+B = next(iter(M))
+H = HoleManager(B)
+H.reset()
+
+while True:
+  H.select()
+  if H.awaitHit():
+    Score += 1
+  H.reset()
